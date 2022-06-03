eb33203e060b1237f7c96e7466dca44c3e7e9de4
Chukwujama <chibuzorhenry200@gmail.com>
2022-06-03 13:51:55 -0700

    Adding 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print 0123456789 using putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n = '0';
+
+while (n <= '9')
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+
+return (0);
+}
