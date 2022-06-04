8621e09ede8a3d19d2ec121249f586395c85d874
Chukwujama <chibuzorhenry200@gmail.com>
2022-06-03 17:18:36 -0700

    Adding 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
 
 /**
- * main - Print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,....)
+ * main - Prints numbers between 00 to 99.
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-int tens;
-int ones;
-int t;
-int o;
+int i, e, g, h, op1, op2;
 
-for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+i = e = g = h = 48;
+while (h < 58)
 {
-for (ones = '0'; ones <= '9'; ones++)
+g = 48;
+while (g < 58)
 {
-for (t = tens; t <= '9'; t++) /*print second of pair*/
+e = 48;
+while (e < 58)
 {
-for (o = ones + 1; o <= '9'; o++)
+i = 48;
+while (i < 58)
 {
-putchar(tens);
-putchar(ones);
+op1 = (h * 10) + g;
+op2 = (e * 10) + i;
+if (op1 < op2)
+{
+putchar(h);
+putchar(g);
 putchar(' ');
-putchar(t);
-putchar(o);
-
-if (!((tens == '9' && ones == '8') && (t == '9' && o == '9')))
-{
+putchar(e);
+putchar(i);
+if (h == 57 && g == 56 && e == 57 && i == 57)
+break;
 putchar(',');
 putchar(' ');
 }
+i++;
 }
-o = '0';
+e++;
 }
+g++;
 }
+h++;
 }
 putchar('\n');
-
 return (0);
 }
