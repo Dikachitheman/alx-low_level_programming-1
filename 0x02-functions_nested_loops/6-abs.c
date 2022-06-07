a7211fe723847a33a970a8557abf721e2d9c59d0
Chukwujama <chibuzorhenry200@gmail.com>
2022-06-07 08:10:42 -0700

    Adding 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ *
+ * @i: To check the integer
+ *
+ * Return: Absolute value
+ */
+
+int _abs(int i)
+{
+	if (i > 0)
+		return (i);
+	else if (i < 0)
+		return (-i);
+	return (0);
+}
