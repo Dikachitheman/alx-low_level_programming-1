1567693a1b8bad3c4eebfc2d20c235d9ec7ec1f9
Chukwujama <chibuzorhenry200@gmail.com>
2022-06-09 07:47:14 -0700

    Adding 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,16 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9.
- * Return: 0
+ * Return: none
  */
 
 void print_numbers(void)
 {
-	int c = 0;
+	int c;
 
-	while (c < 10)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
-		c++;
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
