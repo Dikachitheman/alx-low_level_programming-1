d1d70cd44f1ec13d6541786760d92d4f5abf9a9d
Chukwujama <chibuzorhenry200@gmail.com>
2022-06-24 08:43:40 -0700

    Add

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * main - prints all arguments it recieves.
+ * @argc: number of command line argumnents.
+ * @argv: array of the command line arguments.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: Always 0 unless error when two arguments aren't given, then 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	/* validate input */
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/* multiply two arguments passed via cmd line */
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
