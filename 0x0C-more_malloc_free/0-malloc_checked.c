c5b5565e1a5086434db1a97618ccdba70256fb29
Chukwujama <chibuzorhenry200@gmail.com>
2022-06-30 04:32:30 -0700

    Add

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: unsigned int
+ * Return: a pointer to the allocated memory
+ * if malloc fails, the malloc-checked funtion should cause normal process
+ * termination with a status value of 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+	if (p = NULL)
+		exit(98);
+	return(p);
+}
