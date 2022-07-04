3dfdd9ee73412884f8048b120e4dc9ace8af222e
Chukwujama <chibuzorhenry200@gmail.com>
2022-07-04 16:15:22 -0700

    Add

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 #include <stdio.h>
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 /**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
