daebecd04dd7f752307e3c683fc7aa5c103c9c02
Chukwujama <chibuzorhenry200@gmail.com>
2022-07-04 18:02:15 -0700

    Add

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,18 @@
-free dog
+#include "dog.h"
+
+/**
+ * free_dog - free instance of struct dog
+ * @d: instance to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name) /* free members of struct that exist */
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
