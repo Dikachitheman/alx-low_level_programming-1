b33613422423a4a3eb210be699e96830f070b48d
Chukwujama <chibuzorhenry200@gmail.com>
2022-07-07 05:20:07 -0700

    Add

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-	print_strings(", ", 3, "Jack", "Bauer", "is");
-	print_strings(", ", 4, "Chukwujama", " ", "Chibuzor", "Jamaal");
-	print_strings(", ", 1, "Hey");
-	print_strings(", ", 3, "How're", "you", "doing");
+	print_strings(" ", 3, "Jack", "Bauer", "is");
+	print_strings(" ", 4, "Chukwujama", NULL, "Chibuzor", "Jamaal");
+	print_strings(" ", 1, "Hey");
+	print_strings(" ", 3, "How're", "you", "doing");
 	return (0);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,10 +11,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
+	char *str;
 
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(valist, int));
-		if (separator != NULL && )
+		str = va_arg(valist, char *);
+
+		if (str)
+			printf("%s", str);
+		else
+			printf("nil");
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(valist);
+
+	printf("\n");
+}
