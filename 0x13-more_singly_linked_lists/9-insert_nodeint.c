4b4e9a0dd3ba1678fffd20c8e5e81602c48e519f
Chukwujama <Chukwujama>
2022-07-18 13:49:29 -0700

    Adding

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	/* iterate to 1 before nth index to insert */
 	temp = *head;
-	while (i (idx - 1))
+	while (i < (idx - 1))
 	{
 		temp = temp->next;
 		i++;
