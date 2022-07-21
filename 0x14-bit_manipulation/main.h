6696b45dd920e4f50e9bfc99d597eca19b558f3f
Chukwujama <Chukwujama>
2022-07-21 07:46:22 -0700

    Add

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,7 +9,7 @@
 int _putchar(char c);
 
 /***** Prototypes *****/
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int n, unsigned int index);
