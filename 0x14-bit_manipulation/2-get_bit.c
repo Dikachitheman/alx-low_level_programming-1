8fa5df198b7ba67687edbfa3dad48a0164476b61
Chukwujama <Chukwujama>
2022-07-21 08:56:57 -0700

    Add

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,15 +9,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
-	unsigned int max_bits;
 
-	/* validate index is not out of range */
-	max_bits = (sizeof(unsigned long int) * 8);
-	if (index > max_bits)
+	bit = (n >> index);
+
+	if (index > 32)
 		return (-1);
-
-	/* shift number index places right to find bit */
-	bit = ((n >> index) & 1);
-
-	return (bit);
+	return (bit & 1);
 }
