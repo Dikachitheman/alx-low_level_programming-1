91d9390c6dd383e5da317b49f051c78c3f73f723
Chukwujama <Chukwujama>
2022-07-25 06:42:05 -0700

    Wrk!

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,37 +13,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t n_read, n_wrote;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	/* open */
 	fd = open(filename, 0_RDONLY);
-	if (fd == -1)
+
+	If (fd == -1)
 		return (0);
 
-	/* malloc buffer */
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
 		return (0);
 
-	/* read */
 	n_read = read(fd, buffer, letters);
-	if (n_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	/* write */
 	n_wrote = write(STDOUT_FILENO, buffer, n_read);
-	if (n_wrote == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
 
 	close(fd);
-	return (n_read);
+
+	free(buffer);
+
+	return (n_wrote);
 }
