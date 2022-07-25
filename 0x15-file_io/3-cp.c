56d9beb9a77712984835bcb7128bb133070376e2
Chukwujama <Chukwujama>
2022-07-25 08:08:44 -0700

    d9f

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,7 @@
 #define BUFF_SIZE 1204
 
 /**
- * error-file - checks if files can be opened.
+ * error_file - checks if files can be opened.
  * @file_from: file_from
  * @file_to: file_to
  * @argv: arguments vector
